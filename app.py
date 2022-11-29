b2d136dd827075384e5829e133ad42b138ca9c88
mickey-40 <mickeyarnold53@yahoo.com>
2022-11-28 21:05:28 -0600

    prep for heroku deployment

--- app.py
+++ app.py
@@ -44,14 +44,27 @@ app.register_blueprint(places, url_prefix='/api/v1/places')
 
 app.register_blueprint(user, url_prefix='/api/v1/user')
 
+@app.before_request # use this decorator to cause a function to run before reqs
+def before_request():
 
+    """Connect to the db before each request"""
+    print("you should see this before each request") # optional -- to illustrate that this code runs before each request -- similar to custom middleware in express.  you could also set it up for specific blueprints only.
+    models.DATABASE.connect()
+
+    @after_this_request # use this decorator to Executes a function after this request
+    def after_request(response):
+        """Close the db connetion after each request"""
+        print("you should see this after each request") # optional -- to illustrate that this code runs after each request
+        models.DATABASE.close()
+        return response # go ahead and send response back to client
+                      # (in our case this will be some JSON)
 
 
 # ADD THESE THREE LINES -- because we need to initialize the
 # tables in production too!
-# if os.environ.get('FLASK_ENV') != 'development':
-#   print('\non heroku!')
-#   models.initialize()
+if os.environ.get('FLASK_ENV') != 'development':
+  print('\non heroku!')
+  models.initialize()
 
 # Run the app when the program starts!
 if __name__ == '__main__':
